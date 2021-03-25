4a5fe7c0f20aec21c9ac44063a6485576f803611
Breno Calazans <brenocalazans@gmail.com>
2021-03-25 20:01:57 -0300

    Use sellerDefault as default seller selection

--- react/modules/seller.ts
+++ react/modules/seller.ts
@@ -1,6 +1,12 @@
 import { ProductTypes } from 'vtex.product-context'
 
 export function getSeller(item: ProductTypes.Item) {
+  const defaultSeller = item?.sellers?.find((seller) => seller.sellerDefault)
+
+  if (defaultSeller) {
+    return defaultSeller
+  }
+
   const availableSeller = item?.sellers?.find((seller) => {
     return seller?.commertialOffer?.AvailableQuantity > 0
   })
