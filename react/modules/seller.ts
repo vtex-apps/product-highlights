32403c31129681e91225cf47b7d2dd0a91776cca
Breno Calazans <brenocalazans@gmail.com>
2021-03-30 11:32:56 -0300

    Align seller selection

--- react/modules/seller.ts
+++ react/modules/seller.ts
@@ -3,17 +3,9 @@ import { ProductTypes } from 'vtex.product-context'
 export function getSeller(item: ProductTypes.Item) {
   const defaultSeller = item?.sellers?.find((seller) => seller.sellerDefault)
 
-  if (defaultSeller) {
-    return defaultSeller
-  }
-
-  const availableSeller = item?.sellers?.find((seller) => {
-    return seller?.commertialOffer?.AvailableQuantity > 0
-  })
-
-  if (!availableSeller) {
+  if (!defaultSeller) {
     return item?.sellers?.[0]
   }
 
-  return availableSeller
+  return defaultSeller
 }
