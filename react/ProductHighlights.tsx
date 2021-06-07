e386780c26666c8b9f793df97f5bcd5d086fcfc1
Lucas Castro <lucas.castro@jussi.com.br>
2021-06-07 12:46:07 -0300

    Fix messages

--- react/ProductHighlights.tsx
+++ react/ProductHighlights.tsx
@@ -1,5 +1,6 @@
 import type { FC, ReactNode } from 'react'
 import React, { useContext, useMemo } from 'react'
+import { defineMessages } from 'react-intl'
 import type { ProductTypes } from 'vtex.product-context'
 import { useProduct } from 'vtex.product-context'
 
@@ -123,27 +124,70 @@ function ProductHighlights({
   )
 }
 
+const messages = defineMessages({
+  Filter: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter',
+  },
+  QueryType: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.type',
+  },
+  Collection: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.type.collection',
+  },
+  Promotion: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.type.promotion',
+  },
+  Teaser: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.type.teaser',
+  },
+  Type: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter.type',
+  },
+  Hide: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter.type.hide',
+  },
+  Show: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter.type.show',
+  },
+  HighlightNames: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter.highlight-names',
+  },
+  HighlightName: {
+    defaultMessage: '',
+    id: 'admin/editor.product-highlights.filter.highlight-names.item',
+  },
+})
+
 ProductHighlights.schema = {
   title: 'Product Highlights',
   type: 'object',
   properties: {
     filter: {
-      title: 'Filter',
+      title: messages.Filter.id,
       type: 'object',
       properties: {
         type: {
-          title: 'type',
+          title: messages.Type.id,
           type: 'string',
           enum: ['hide', 'show'],
-          enumNames: ['hide', 'show'],
+          enumNames: [messages.Hide.id, messages.Show.id],
         },
         highlightNames: {
-          title: 'Highlight Names',
+          title: messages.HighlightNames.id,
           type: 'array',
           minItems: 0,
           maxItems: 5,
           items: {
-            title: 'Highlight name',
+            title: messages.HighlightName.id,
             type: 'string',
             default: '',
           },
@@ -151,10 +195,14 @@ ProductHighlights.schema = {
       },
     },
     type: {
-      title: 'Query Type',
+      title: 'admin/editor.product-highlights.type',
       type: 'string',
       enum: ['collection', 'promotion', 'teaser'],
-      enumNames: ['collection', 'promotion', 'teaser'],
+      enumNames: [
+        messages.Collection.id,
+        messages.Promotion.id,
+        messages.Teaser.id,
+      ],
     },
   },
 }
